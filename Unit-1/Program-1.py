c3a0643ab057b015efccac31877ddc4b8f64af87
Suhana Divan <suhudivan15@gmail.com>
2026-08-17 10:21:58 +0530

    Implement basic input/output demonstration program
    Added a program to demonstrate basic input and output operations in Python.

--- Unit-1/Program-1.py
+++ Unit-1/Program-1.py
@@ -1,1 +1,5 @@
-
+#Write a program to demonstrate basic input and output operstion in python.
+nm=input("Enter your name : ")
+rno=input("Enter your roll number : ")
+print("Your name is ",nm)
+print("Your name is ",rno)
